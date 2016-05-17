9bcd1f8afbaa243fb68cbef8cad7f3b11aa71a43
KrishnaChaitanya <chait.amjuri@gmail.com>
2016-05-17 23:18:52 +0530

    Initial Commit

---
+++ iMammoth for reddit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iMammoth for reddit
+//
+//  Created by KrishnaChaitanya Amjuri on 17/05/16.
+//  Copyright © 2016 KrishnaChaitanya Amjuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
